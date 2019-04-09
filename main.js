1e78453b935423d101b73da6d6675280f6891013
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-04-08 21:03:17 -0300

    boton cerrar

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var menuHamburguesa = document.getElementById('botonBurgu');
 var menuDesplegado = document.getElementById('navVertical');
-
+var botonCerrar = document.getElementById('botonCerrar');
 
 menuHamburguesa.onclick = function(){
 
     menuDesplegado.classList.remove('displayNone');
     menuHamburguesa.classList.add('displayNone');
 }
+
+botonCerrar.onclick = function(){
+
+    menuDesplegado.classList.add('displayNone');
+
+}
+
+
